a507259d71b3f860c2a28f4fe483caeb134ab751
Luay Abbas <abbasluay1981@gmail.com>
2023-07-31 23:32:58 +0200

    add-styles

---
+++ index.js
@@ -0,0 +1,13 @@
+const frame = document.querySelector(".frame")
+
+let colorsArr=[
+    "#f44336","#e91e63","#9c27b0","#673ab7","#3f51b5","#2196f3","#03a9f4","#00bcd4",
+    "#009688","#4caf50","#8bc34a","#cddc39","#ffeb3b","#ffc107","#ff9800","#ff5722"
+]
+
+setInterval(() => {
+    index= Math.floor(Math.random()* colorsArr.length)
+   let color= colorsArr[index]
+    frame.style= `--b:${color}`
+}, 2000);
+
